7d6bf3c7b47db48bb5320dcec05c65131e0bbce3
@NSHU OJH@ <ojhaanshu91@gmail.com>
2020-11-24 10:20:12 +0530

    Create 283_move_zeros.py
    Move 0 at last without array modification with constant space.

---
+++ 283_move_zeros.py
@@ -0,0 +1,20 @@
+"""
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Example:
+
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Note:
+
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+"""
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        append_times = nums.count(0)
+        for elem in range (append_times):
+            nums.remove(0)
+            nums.append(0)
+        return nums
