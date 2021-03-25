e4680c5a2b97fa0abd92895d9caaf35dbc06fc7b
@NSHU OJH@ <ojhaanshu91@gmail.com>
2021-03-25 12:16:37 +0530

    Update 724_pivot_or_equilibrium_idx.py

--- 724_pivot_or_equilibrium_idx.py
+++ 724_pivot_or_equilibrium_idx.py
@@ -41,3 +41,18 @@ class Solution(object):
                 return idx
             left_sum += elem
         return -1
+# BALANCE WEIGHT O(N) TC
+class Solution(object):
+    def pivotIndex(self, nums):
+        # Initialization:
+        # Left hand side be empty, and
+        # Right hand side holds all weights.
+        total_weight_on_left, total_weight_on_right = 0, sum(nums)
+        for idx, current_weight in enumerate(nums):
+            total_weight_on_right -= current_weight
+            if total_weight_on_left == total_weight_on_right:
+                # balance is met on both sides
+                # i.e., sum( nums[ :idx] ) == sum( nums[idx+1: ] )
+                return idx
+            total_weight_on_left += current_weight
+        return -1
