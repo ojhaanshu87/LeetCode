3a55aa5e64387afadf4a34b92a0216b326420ae0
@NSHU OJH@ <ojhaanshu91@gmail.com>
2021-03-30 12:17:39 +0530

    Update 49_group_anagram.py

--- 49_group_anagram.py
+++ 49_group_anagram.py
@@ -39,3 +39,26 @@ class Solution(object):
                 count[ord(c) - ord('a')] += 1
             ans[tuple(count)].append(s)
         return ans.values()
+
+# Hashmap and Sorting 
+
+'''
+We create a dictionary and for each word in the input array, we add a key to the dictionary if the sorted version of the word doesn't already exist in the list of keys.
+The key then becomes the sorted version of the word, and the value for the key is an array that stores each anagram of the key.
+i.e. for every next word that is an anagram, we would sort the word, find the key that is equal to the sorted form,
+and add the original word to the list of values for the key.
+At the end of it, we just add every value in the dictionary to the final array.
+'''
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        temp, res = {}, []
+        for word in strs:
+            sortedWord = ''.join(sorted(word))
+            if sortedWord not in temp:
+                temp[sortedWord] = [word]
+            else:
+                temp[sortedWord].append(word)       
+        for value in temp.values():
+            res.append(value)
+        return res
