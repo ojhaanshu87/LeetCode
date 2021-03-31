959b25318a6135e2ed35411920b871502095781c
@NSHU OJH@ <ojhaanshu91@gmail.com>
2021-03-31 11:16:35 +0530

    Create 704_binary_search.py

---
+++ 704_binary_search.py
@@ -0,0 +1,27 @@
+'''
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums.
+If target exists, then return its index. Otherwise, return -1.
+
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+'''
+
+class Solution(object):
+    def search(self, nums, target):
+        low, high = 0, len(nums) - 1
+        while low  <= high:
+            mid = low + (high - low) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
