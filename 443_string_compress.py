88a64563cbdce46ed920a3fad9f12e5616754ba8
@NSHU OJH@ <ojhaanshu91@gmail.com>
2021-04-22 08:17:26 +0530

    Create 443_string_compress.py

---
+++ 443_string_compress.py
@@ -0,0 +1,65 @@
+'''
+Given an array of characters chars, compress it using the following algorithm:
+
+Begin with an empty string s. For each group of consecutive repeating characters in chars:
+
+If the group's length is 1, append the character to s.
+Otherwise, append the character followed by the group's length.
+The compressed string s should not be returned separately, but instead be stored in the input character array chars.
+Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+
+After you are done modifying the input array, return the new length of the array.
+
+Follow up:
+Could you solve it using only O(1) extra space?
+
+ 
+
+Example 1:
+
+Input: chars = ["a","a","b","b","c","c","c"]
+Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+Example 2:
+
+Input: chars = ["a"]
+Output: Return 1, and the first character of the input array should be: ["a"]
+Explanation: The only group is "a", which remains uncompressed since it's a single character.
+Example 3:
+
+Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
+Example 4:
+
+Input: chars = ["a","a","a","b","b","a","a"]
+Output: Return 6, and the first 6 characters of the input array should be: ["a","3","b","2","a","2"].
+Explanation: The groups are "aaa", "bb", and "aa". This compresses to "a3b2a2". Note that each group is independent even if two groups have the same character.
+'''
+
+# O(N) TC and O(1) Space
+
+'''
+Algorithm
+
+Let's maintain anchor, the start position of the contiguous group of characters we are currently reading.
+
+Now, let's read from left to right. We know that we must be at the end of the block when we are at the last character,
+or when the next character is different from the current character.
+When we are at the end of a group, we will write the result of that group down using our write head. 
+chars[anchor] will be the correct character, and the length (if greater than 1) will be read - anchor + 1. We will write the digits of that number to the array.
+'''
+
+class Solution(object):
+    def compress(self, chars):
+        anchor, write = 0, 0
+        for read, char in enumerate(chars):
+            if read + 1 == len(chars) or chars[read + 1] != char:
+                chars[write] = chars[anchor]
+                write += 1
+                if read > anchor:
+                    for digit in str(read - anchor + 1):
+                        chars[write] = digit
+                        write += 1
+                anchor = read + 1
+        return write
