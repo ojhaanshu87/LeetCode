207dd20b229787bbe0a786897d00e5d26c4dbec6
@NSHU OJH@ <ojhaanshu91@gmail.com>
2021-05-24 20:29:45 +0530

    Create 213_house_robber_circle.py

---
+++ 213_house_robber_circle.py
@@ -0,0 +1,40 @@
+'''
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. 
+All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, 
+adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+Example 1:
+Input: nums = [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+
+Example 2:
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+
+Example 3:
+Input: nums = [0]
+Output: 0
+'''
+class Solution(object):
+    def rob(self, nums):
+        if len(nums) == 1:
+            return nums[0]
+        elif len(nums) == 2:
+            return max(nums[0], nums[1])
+        else:
+            prev, curr = nums[0], max(nums[0], nums[1])
+            for elem in range(2, len(nums) - 1):
+                prev, curr = curr, max(nums[elem] + prev, curr)
+            max_num = curr
+
+            prev, curr = 0, nums[1]
+            for elem in range(2, len(nums)):
+                prev, curr = curr, max(nums[elem] + prev, curr)
+            return max(max_num, curr)
+            
+        
